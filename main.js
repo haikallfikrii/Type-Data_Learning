aac44305c510def97e8bf44c948d9848552436fe
haikallfikrii <fikri789haikal@gmail.com>
2024-05-03 09:16:15 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// 1. TYPE DATA
+// STRING
+/*let firstName = "Fikri";
+let lastName = "Haikal";
+
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`); */
+
+//NUMBER
+let age = 21;
+let height = 162.4;
+console.log(typeof age, typeof height);
+
+//BOLEAN
+let isMarried = false;
+let isMale = true;
+console.log(typeof isMarried, typeof isMale);
+
+//TYPE DATA NON PRIMITIF
+const buah = ["nanas", "jeruk", "apel", "khuldi"];
+
+const nilai = [11, 12, 13, 14, 15];
+const arr = ["Nanas", 91, true];
+console.log(arr);
